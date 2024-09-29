03ca9973d734cccfe97f2897853778988ed2745c
SanBor441 <vinogorov.aleksander@gmail.com>
2024-09-29 21:11:12 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,9 @@
+complete_home_work = "12"
+print(complete_home_work)
+number_of_hour_spent = "1.5"
+print(number_of_hour_spent)
+name_course = "Python"
+print(name_course)
+time_for_one_task = 1.5 / 12
+print(time_for_one_task)
+print(complete_home_work,number_of_hour_spent,name_course,time_for_one_task)
